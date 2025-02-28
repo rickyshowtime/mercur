633d015588115a07ffe2b3be4d420589ba054ed2
Rickylabs <eric.chautems@gmail.com>
2025-02-28 16:45:54 +0100

    fix: module is not defined error
    moved logic to app

--- apps/backend/src/modules/requests/service.ts
+++ apps/backend/src/modules/requests/service.ts
@@ -1,40 +1,11 @@
 import { MedusaService } from '@medusajs/framework/utils'
 
-import {
-  acceptProductCategoryRequestWorkflow,
-  acceptProductCollectionRequestWorkflow,
-  acceptProductRequestWorkflow,
-  acceptReviewRemoveRequestWorkflow,
-  acceptSellerCreationRequestWorkflow
-} from '../../workflows/requests/workflows'
 import { Request } from './models'
 
 class RequestsModuleService extends MedusaService({
   Request
 }) {
-  static getWorkflowByType(type: string) {
-    if (type === 'product_collection') {
-      return acceptProductCollectionRequestWorkflow
-    }
 
-    if (type === 'product_category') {
-      return acceptProductCategoryRequestWorkflow
-    }
-
-    if (type === 'product') {
-      return acceptProductRequestWorkflow
-    }
-
-    if (type === 'seller') {
-      return acceptSellerCreationRequestWorkflow
-    }
-
-    if (type === 'review_remove') {
-      return acceptReviewRemoveRequestWorkflow
-    }
-
-    return null
-  }
 }
 
 export default RequestsModuleService
